d193e28e60b61a014f56df687883e7aad1136d38
johnson <johnson.chen@program.com.tw>
2020-11-16 17:34:12 +0800

    initial commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/chnejohnson/prom_client
+
+go 1.15
+
+require (
+	github.com/prometheus/client_golang v1.8.0 // indirect
+	github.com/prometheus/common v0.15.0 // indirect
+	golang.org/x/sys v0.0.0-20201113233024-12cec1faf1ba // indirect
+	google.golang.org/protobuf v1.25.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+var addr = flag.String("listen-address", ":8080", "The address to listen on for HTTP requests.")
+
+var (
+	cpuTemp = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "cpu_temperature_celsius",
+		Help: "Current temperature of the CPU.",
+	})
+	hdFailures = prometheus.NewCounterVec(
+		prometheus.CounterOpts{
+			Name: "hd_errors_total",
+			Help: "Number of hard-disk errors.",
+		},
+		[]string{"device"},
+	)
+)
+
+func init() {
+	// Metrics have to be registered to be exposed:
+	prometheus.MustRegister(cpuTemp)
+	prometheus.MustRegister(hdFailures)
+}
+
+func main() {
+	flag.Parse()
+	cpuTemp.Set(65.3)
+	http.Handle("/metrics", promhttp.Handler())
+	fmt.Printf("Server listening up on port %s\n", *addr)
+	log.Fatal(http.ListenAndServe(*addr, nil))
+}
